650ee968546172280f2de6a52bc89f8ed3a85ab3
KiranA994 <kiranagrasala13@gmail.com>
2024-04-26 11:22:13 +0530

    card added to cart, home and wishlist page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,70 @@
+import { faTrash, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Cart() {
   return (
-    <div>Cart</div>
+    <>
+    <div className="row my-5">
+      <div className="col-md-1"></div>
+      <div className="col-md-7 mt-5 mb-5" style={{overflowX:'auto'}}>
+        <table className='table shadow w-100'>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product</th>
+              <th>Image</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+<tr>
+  <td>1</td>
+  <td>iPhone-9</td>
+  <td>
+    <img src="" alt="" style={{width:'150px',height:'150px'}}/>
+  </td>
+  <td>$ 549</td>
+  <td>
+  <button className='btn btn-outline-danger rounded'>
+  <FontAwesomeIcon icon={faTrash} style={{color: "#ff0000",}} />
+  </button>
+  </td>
+</tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="col-md-3 mt-5" >
+        <div className='shadow p-4'>
+          <h1 className='text-primary mb-3'>Cart Summary</h1>
+          <h4>Total number of Products: <span className='text-warning'>3</span></h4>
+          <h4 className=''>Total Price : <span className='text-warning'>$59</span></h4>
+          <button className='btn btn-success rounded mt-4 mb-3 w-100'>Checkout</button>
+        </div>
+      </div>
+      <div className="col-md-1"></div>
+
+       {/* <div className='d-flex justify-content-center align-items-center w-100 mb-5'>
+  <div className="col-md-4"></div>
+  <div className="col-md-4 d-flex justify-content-center align-items-center flex-column">
+    <img src="https://cdn.dribbble.com/users/5107895/screenshots/14532312/media/a7e6c2e9333d0989e3a54c95dd8321d7.gif" alt="empty cart"
+    style={{width:'300px',height: '300px'}} />
+  <h2 className='text-danger mt-4'>Your Cart is empty</h2>
+<Link to={'/'}>
+    <button className='btn btn-success p-3 rounded mt-2'>
+    <FontAwesomeIcon icon={faArrowLeft} className='me-3'/>
+    Back To Home</button>
+</Link>
+  </div>
+  </div> */}
+    </div>
+
+
+
+
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,204 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import useFetch from '../Hooks/useFetch';
 
 function Home() {
+
+  const data = useFetch('https://dummyjson.com/products')
+  console.log(data);
   return (
-    <div>
-      Home
+    <>
+    <div className='d-flex p-5'>
+    <div className='row'>
+    <div className="col-md-3 mt-3 ms-2">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <p style={{fontWeight:'bold'}}>Price : 50$</p>
+          <div className='d-flex justify-content-between'>
+          <Button className='rounded' variant='outline-danger'>
+          <FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",backgroundColor: 'white'}} />
+          </Button>
+          <Button className='rounded' variant='outline-success'>
+          <FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff33",backgroundColor:'white'}} />
+          </Button>
+          </div>
+
+        
+      </Card.Body>
+    </Card>  
+      </div>
+      <div className="col-md-3 mt-3">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <p style={{fontWeight:'bold'}}>Price : 50$</p>
+          <div className='d-flex justify-content-between'>
+          <Button className='rounded' variant='outline-danger'>
+          <FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",backgroundColor: 'white'}} />
+          </Button>
+          <Button className='rounded' variant='outline-success'>
+          <FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff33",backgroundColor:'white'}} />
+          </Button>
+          </div>
+
+        
+      </Card.Body>
+    </Card>  
+      </div>
+      <div className="col-md-3 mt-3">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <p style={{fontWeight:'bold'}}>Price : 50$</p>
+          <div className='d-flex justify-content-between'>
+          <Button className='rounded' variant='outline-danger'>
+          <FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",backgroundColor: 'white'}} />
+          </Button>
+          <Button className='rounded' variant='outline-success'>
+          <FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff33",backgroundColor:'white'}} />
+          </Button>
+          </div>
+
+        
+      </Card.Body>
+    </Card>  
+      </div>      <div className="col-md-3 mt-3">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <p style={{fontWeight:'bold'}}>Price : 50$</p>
+          <div className='d-flex justify-content-between'>
+          <Button className='rounded' variant='outline-danger'>
+          <FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",backgroundColor: 'white'}} />
+          </Button>
+          <Button className='rounded' variant='outline-success'>
+          <FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff33",backgroundColor:'white'}} />
+          </Button>
+          </div>
+
+        
+      </Card.Body>
+    </Card>  
+      </div>      <div className="col-md-3 mt-3">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <p style={{fontWeight:'bold'}}>Price : 50$</p>
+          <div className='d-flex justify-content-between'>
+          <Button className='rounded' variant='outline-danger'>
+          <FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",backgroundColor: 'white'}} />
+          </Button>
+          <Button className='rounded' variant='outline-success'>
+          <FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff33",backgroundColor:'white'}} />
+          </Button>
+          </div>
+
+        
+      </Card.Body>
+    </Card>  
+      </div>      <div className="col-md-3 mt-3">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <p style={{fontWeight:'bold'}}>Price : 50$</p>
+          <div className='d-flex justify-content-between'>
+          <Button className='rounded' variant='outline-danger'>
+          <FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",backgroundColor: 'white'}} />
+          </Button>
+          <Button className='rounded' variant='outline-success'>
+          <FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff33",backgroundColor:'white'}} />
+          </Button>
+          </div>
+
+        
+      </Card.Body>
+    </Card>  
+      </div>      <div className="col-md-3 mt-3">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <p style={{fontWeight:'bold'}}>Price : 50$</p>
+          <div className='d-flex justify-content-between'>
+          <Button className='rounded' variant='outline-danger'>
+          <FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",backgroundColor: 'white'}} />
+          </Button>
+          <Button className='rounded' variant='outline-success'>
+          <FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff33",backgroundColor:'white'}} />
+          </Button>
+          </div>
+
+        
+      </Card.Body>
+    </Card>  
+      </div>
+      <div className="col-md-3 mt-3">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <p style={{fontWeight:'bold'}}>Price : 50$</p>
+          <div className='d-flex justify-content-between'>
+          <Button className='rounded' variant='outline-danger'>
+          <FontAwesomeIcon icon={faHeart} style={{color: "#ff0000",backgroundColor: 'white'}} />
+          </Button>
+          <Button className='rounded' variant='outline-success'>
+          <FontAwesomeIcon icon={faCartShopping} style={{color: "#00ff33",backgroundColor:'white'}} />
+          </Button>
+          </div>
+
+        
+      </Card.Body>
+    </Card>  
+      </div>
+
     </div>
+    </div>
+  
+    </>
+    
   )
 }
 
